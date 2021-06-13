e37bc3906dbf333ce7f71b8407a1b1979c36f36c
caio reis <reis.caio@gmail.com>
2021-06-13 20:11:18 -0300

    cópia funcionando com strcpy

--- main.c
+++ main.c
@@ -13,12 +13,12 @@
 // cada nó da lista ligada composta pela struct operaçao contém a próxima operação, a quantidade e o valor.
 typedef struct NohOperacao{
     struct NohOperacao* nohOperacaoAnterior;
-    char *quantidade; // tudo vai ser armazenado como string até o momento de fazer as operações matemática, a partir daí fazemos as conversões e desconversões
-    char *valor;
+    char quantidade[10]; // tudo vai ser armazenado como string até o momento de fazer as operações matemática, a partir daí fazemos as conversões e desconversões
+    char valor[10];
 }NohOperacao;
 
 typedef struct Titulo{
-    char* sigla; // const char* é utilizado para que a string possa ser retornada de uma função
+    char sigla[10]; // const char* é utilizado para que a string possa ser retornada de uma função
     NohOperacao ultimaCompra;
     NohOperacao ultimaVenda;
 }Titulo;
@@ -28,57 +28,101 @@ Titulo *listaDinamicaPrincipal;
 int tamanhoDaListaPrincipal = TAMANHO_INICIAL;
 int espacoPrincipalUtilizado = 0;
 
-void salvarOferta(char *sigla, char *tipo, char *valor, char *quantidade){
-    NohOperacao novaOperacao;
-    int existeTitulo = 0;
-
-//  itera sobre a estrutura principal buscando pra ver se já há título salvo com a mesma sigla
-//  pois caso haja, o comportamento é armazenar a nova operação ao final da lista encadeada e bota ela para apontar para a operação anterior
-    for(int iterador = 0; iterador < espacoPrincipalUtilizado; iterador++){
-        if(listaDinamicaPrincipal[iterador].sigla == sigla){
-            existeTitulo = 1;
-           if(tolower(tipo[0])=='c'){
-               novaOperacao.valor = valor;
-               novaOperacao.quantidade = quantidade;
-               novaOperacao.nohOperacaoAnterior = &listaDinamicaPrincipal[iterador].ultimaCompra; // bota a nova operação para apontar para a última operação daquele tipo (c ou v) adicionada anteriormente
-               listaDinamicaPrincipal[iterador].ultimaCompra = novaOperacao; // atualiza a última operação daquele título para ser a nova operação
-           }
-           else{
-               novaOperacao.valor = valor;
-               novaOperacao.quantidade = quantidade;
-               novaOperacao.nohOperacaoAnterior = &listaDinamicaPrincipal[iterador].ultimaVenda;
-               listaDinamicaPrincipal[iterador].ultimaVenda = novaOperacao;
-           }
-       }
-   }
-
-    if(!existeTitulo){
-//      primeiro verificamos se há espaço na estrutura principal para receber mais um novo título, caso não haja será preciso dobrar o tamanho da estrutura
-//      para isso vamos atualizar a variável global que armazena o tamanho da estrutura principal e dobrar o tamanho da estrutura principal
-        if(tamanhoDaListaPrincipal==espacoPrincipalUtilizado){
-            tamanhoDaListaPrincipal = tamanhoDaListaPrincipal * 2;
-            listaDinamicaPrincipal = (Titulo *) realloc(listaDinamicaPrincipal, tamanhoDaListaPrincipal * sizeof(Titulo) );
-        }
-
-//      como não existe o título, será preciso criar um apontando para NULL já que não há nenhuma operação prévia
-//      a criação do título envolve além da adição da sigla, a adição da última operação para cada tipo, daí inserimos como última operação a nova operação
-        Titulo novoTitulo;
-        novoTitulo.sigla = sigla;
-        novaOperacao.nohOperacaoAnterior = NULL;
-        if(tolower(tipo[0])=='c'){
-            novaOperacao.valor = valor;
-            novaOperacao.quantidade = quantidade;
-            novoTitulo.ultimaCompra = novaOperacao;
-        }
-        else{
-            novaOperacao.valor = valor;
-            novaOperacao.quantidade = quantidade;
-            novoTitulo.ultimaVenda = novaOperacao;
-        }
-
-//      ao final depois do título já montado precisamos apenas adicionar ele na nossa estrutura principal
-        listaDinamicaPrincipal[espacoPrincipalUtilizado++] = novoTitulo;
+Titulo *buscaTitulo(char *sigla, int iterador) {
+    if (listaDinamicaPrincipal[iterador].sigla == sigla){
+        return &listaDinamicaPrincipal[iterador];
     }
+    if(iterador>=espacoPrincipalUtilizado) {
+        return NULL;
+    }
+    return buscaTitulo(sigla, ++iterador);
+}
+
+void salvarOferta(char *sigla, const char *tipo, char *valor, char *quantidade){
+    // tentativa 2:
+//    Titulo *tituloExistente = buscaTitulo(sigla, 0);
+//    if(tituloExistente && tipo[0]=='c'){
+//        NohOperacao novaCompra;
+//        novaCompra.quantidade = quantidade;
+//        novaCompra.valor = valor;
+//        novaCompra.nohOperacaoAnterior = &tituloExistente->ultimaCompra;
+//        tituloExistente->ultimaCompra = novaCompra;
+//    }
+//    if(tituloExistente && tipo[0]=='v'){
+//        NohOperacao novaVenda;
+//        novaVenda.quantidade = quantidade;
+//        novaVenda.valor = valor;
+//        novaVenda.nohOperacaoAnterior = &tituloExistente->ultimaVenda;
+//        tituloExistente->ultimaVenda = novaVenda;
+//    }
+//    if(!tituloExistente){
+        strcpy(listaDinamicaPrincipal[espacoPrincipalUtilizado].sigla, sigla); // os valores que estou recebendo como argumentos estão mudando, então as atribuições ficam todas erradas, é preciso usar strcpy para funcionar
+        if(tipo[0]=='c'){
+            strcpy(listaDinamicaPrincipal[espacoPrincipalUtilizado].ultimaCompra.quantidade,quantidade);
+            strcpy(listaDinamicaPrincipal[espacoPrincipalUtilizado].ultimaCompra.valor,valor);
+            listaDinamicaPrincipal[espacoPrincipalUtilizado].ultimaCompra.nohOperacaoAnterior = NULL;
+        }
+        if(tipo[0]=='v'){
+            strcpy(listaDinamicaPrincipal[espacoPrincipalUtilizado].ultimaVenda.quantidade,quantidade);
+            strcpy(listaDinamicaPrincipal[espacoPrincipalUtilizado].ultimaVenda.valor,valor);
+            listaDinamicaPrincipal[espacoPrincipalUtilizado].ultimaVenda.nohOperacaoAnterior = NULL;
+        }
+        espacoPrincipalUtilizado = espacoPrincipalUtilizado + 1;
+//    }
+
+
+
+
+//    // tentativa 1:
+//    NohOperacao novaOperacao;
+//    int existeTitulo = 0;
+////  itera sobre a estrutura principal buscando pra ver se já há título salvo com a mesma sigla
+////  pois caso haja, o comportamento é armazenar a nova operação ao final da lista encadeada e bota ela para apontar para a operação anterior
+//    for(int iterador = 0; iterador < espacoPrincipalUtilizado; iterador++){
+//        if(listaDinamicaPrincipal[iterador].sigla == sigla){
+//            existeTitulo = 1;
+//           if(tolower(tipo[0])=='c'){
+//               novaOperacao.valor = valor;
+//               novaOperacao.quantidade = quantidade;
+//               novaOperacao.nohOperacaoAnterior = &listaDinamicaPrincipal[iterador].ultimaCompra; // bota a nova operação para apontar para a última operação daquele tipo (c ou v) adicionada anteriormente
+//               listaDinamicaPrincipal[iterador].ultimaCompra = novaOperacao; // atualiza a última operação daquele título para ser a nova operação
+//           }
+//           else{
+//               novaOperacao.valor = valor;
+//               novaOperacao.quantidade = quantidade;
+//               novaOperacao.nohOperacaoAnterior = &listaDinamicaPrincipal[iterador].ultimaVenda;
+//               listaDinamicaPrincipal[iterador].ultimaVenda = novaOperacao;
+//           }
+//       }
+//   }
+//
+//    if(!existeTitulo){
+////      primeiro verificamos se há espaço na estrutura principal para receber mais um novo título, caso não haja será preciso dobrar o tamanho da estrutura
+////      para isso vamos atualizar a variável global que armazena o tamanho da estrutura principal e dobrar o tamanho da estrutura principal
+//        if(tamanhoDaListaPrincipal==espacoPrincipalUtilizado){
+//            tamanhoDaListaPrincipal = tamanhoDaListaPrincipal * 2;
+//            listaDinamicaPrincipal = (Titulo *) realloc(listaDinamicaPrincipal, tamanhoDaListaPrincipal * sizeof(Titulo) );
+//        }
+//
+////      como não existe o título, será preciso criar um apontando para NULL já que não há nenhuma operação prévia
+////      a criação do título envolve além da adição da sigla, a adição da última operação para cada tipo, daí inserimos como última operação a nova operação
+//        Titulo novoTitulo;
+//        novoTitulo.sigla = sigla;
+//        novaOperacao.nohOperacaoAnterior = NULL;
+//        if(tolower(tipo[0])=='c'){
+//            novaOperacao.valor = valor;
+//            novaOperacao.quantidade = quantidade;
+//            novoTitulo.ultimaCompra = novaOperacao;
+//        }
+//        else{
+//            novaOperacao.valor = valor;
+//            novaOperacao.quantidade = quantidade;
+//            novoTitulo.ultimaVenda = novaOperacao;
+//        }
+//
+////      ao final depois do título já montado precisamos apenas adicionar ele na nossa estrutura principal
+//        listaDinamicaPrincipal[espacoPrincipalUtilizado++] = novoTitulo;
+//    }
 }
 
 //char identificaTipo(){
@@ -127,22 +171,22 @@ void inserirOfertaDoUsuario() {
 }
 
 
-void listarOfertas(){
-    for(int iterador = 0; iterador < espacoPrincipalUtilizado; iterador++){
-        printf("Sigla: %s\n", listaDinamicaPrincipal[iterador].sigla);
-        printf("Compras:\n");
-        while(listaDinamicaPrincipal[iterador].ultimaCompra.nohOperacaoAnterior!=NULL){
-            printf(" valor: %s, quantidade: %s\n", listaDinamicaPrincipal[iterador].ultimaCompra.valor, listaDinamicaPrincipal[iterador].ultimaCompra.quantidade);
-            listaDinamicaPrincipal[iterador].ultimaCompra.nohOperacaoAnterior = listaDinamicaPrincipal[iterador].ultimaCompra.nohOperacaoAnterior->nohOperacaoAnterior;
-        }
-
-        printf("Vendas:\n");
-        while(listaDinamicaPrincipal[iterador].ultimaVenda.nohOperacaoAnterior!=NULL){
-            printf(" valor: %s, quantidade: %s\n", listaDinamicaPrincipal[iterador].ultimaVenda.valor, listaDinamicaPrincipal[iterador].ultimaVenda.quantidade);
-            listaDinamicaPrincipal[iterador].ultimaVenda.nohOperacaoAnterior = listaDinamicaPrincipal[iterador].ultimaVenda.nohOperacaoAnterior->nohOperacaoAnterior;
-        }
-    }
-}
+//void listarOfertas(){
+//    for(int iterador = 0; iterador < espacoPrincipalUtilizado; iterador++){
+//        printf("Sigla: %s\n", listaDinamicaPrincipal[iterador].sigla);
+//        printf("Compras:\n");
+//        while(listaDinamicaPrincipal[iterador].ultimaCompra.nohOperacaoAnterior!=NULL){
+//            printf(" valor: %s, quantidade: %s\n", listaDinamicaPrincipal[iterador].ultimaCompra.valor, listaDinamicaPrincipal[iterador].ultimaCompra.quantidade);
+//            listaDinamicaPrincipal[iterador].ultimaCompra.nohOperacaoAnterior = listaDinamicaPrincipal[iterador].ultimaCompra.nohOperacaoAnterior->nohOperacaoAnterior;
+//        }
+//
+//        printf("Vendas:\n");
+//        while(listaDinamicaPrincipal[iterador].ultimaVenda.nohOperacaoAnterior!=NULL){
+//            printf(" valor: %s, quantidade: %s\n", listaDinamicaPrincipal[iterador].ultimaVenda.valor, listaDinamicaPrincipal[iterador].ultimaVenda.quantidade);
+//            listaDinamicaPrincipal[iterador].ultimaVenda.nohOperacaoAnterior = listaDinamicaPrincipal[iterador].ultimaVenda.nohOperacaoAnterior->nohOperacaoAnterior;
+//        }
+//    }
+//}
 
 
 void carregarOfertasDeArquivo(){
@@ -156,7 +200,7 @@ void carregarOfertasDeArquivo(){
     if (!arquivo){
         arquivo = fopen("homebroker-database.csv", "r");
         if(!arquivo){
-            printf("nao foi possivel ler o arquivo\n");
+            printf("Nao foi possivel ler o arquivo\n");
             exit(1);
         }
     }
@@ -216,6 +260,7 @@ int main() {
     listaDinamicaPrincipal = (Titulo *) malloc(tamanhoDaListaPrincipal * sizeof(Titulo));
     carregarOfertasDeArquivo();
     printf("sigla:%s\nqtd de compra:%s\nvalor compra:%s\nvalor venda:%s\nquantidade venda:%s\n",listaDinamicaPrincipal[0].sigla, listaDinamicaPrincipal[0].ultimaCompra.quantidade, listaDinamicaPrincipal[0].ultimaCompra.valor, listaDinamicaPrincipal[0].ultimaVenda.valor, listaDinamicaPrincipal[0].ultimaVenda.quantidade);
+    printf("sigla:%s\nqtd de compra:%s\nvalor compra:%s\nvalor venda:%s\nquantidade venda:%s\n",listaDinamicaPrincipal[1].sigla, listaDinamicaPrincipal[1].ultimaCompra.quantidade, listaDinamicaPrincipal[1].ultimaCompra.valor, listaDinamicaPrincipal[1].ultimaVenda.valor, listaDinamicaPrincipal[1].ultimaVenda.quantidade);
 //    listarOfertas();
 //    exibirMenu();
     free(listaDinamicaPrincipal);
